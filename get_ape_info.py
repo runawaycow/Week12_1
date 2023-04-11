3a2fb5c4ed7bd49cb7ab6b5b2e14a54efd6d805f
Dan Luo <luo_dana@yahoo.com>
2023-04-12 04:13:57 +1000

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -26,8 +26,41 @@ def get_ape_info(apeID):
 	assert 1 <= apeID, f"{apeID} must be at least 1"
 
 	data = {'owner': "", 'image': "", 'eyes': "" }
+
+	############################
+	#Get Contract ABI from etherscan
+	ABI_ENDPOINT = 'https://api.etherscan.io/api?module=contract&action=getabi&address='
+	try:
+		response = requests.get( f"{ABI_ENDPOINT}{contract_address}", timeout = 20 )	
+		abi = response.json()
+	except Exception as e:
+		print( f"Failed to get {contract_address} from {ABI_ENDPOINT}" )
+		print( e )
+
+	bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
 	
-	#YOUR CODE HERE	
+	############################
+	#Connect to an Ethereum node
+	token = "Mwb3juVAfI1g2RmA1JCGdYk-2_BmFrnLOtbomP1oDa4"
+	api_url = f"https://c2emjgrvmi7cabd41mpg.bdnodes.net?auth={token}"
+	provider = HTTPProvider(api_url)
+	web3 = Web3(provider)
+
+	contract = web3.eth.contract(address=contract_address,abi=abi)
+
+    # Call the contract function to get the owner of the ape
+    owner = contract.functions.ownerOf(apeID).call()
+
+    # Call the contract function to get the image URL of the ape
+    image = contract.functions.tokenURI(apeID).call()
+
+    # Call the contract function to get the eye color of the ape
+    eyes = contract.functions.getApeDetails(apeID).call()[3]
+
+    # Fill the data dictionary with the results
+    data['owner'] = owner
+    data['image'] = image
+    data['eyes'] = eyes
 
 	assert isinstance(data,dict), f'get_ape_info{apeID} should return a dict' 
 	assert all( [a in data.keys() for a in ['owner','image','eyes']] ), f"return value should include the keys 'owner','image' and 'eyes'"
